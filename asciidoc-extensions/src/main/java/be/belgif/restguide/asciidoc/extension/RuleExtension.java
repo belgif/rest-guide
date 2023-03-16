d4136cf5e4bb5ab68983cc9b57532276e667e77a
Peter Van den Bosch <peter.vandenbosch@ksz-bcss.fgov.be>
2023-03-16 09:30:15 +0100

    Stable rule identifiers (#111)
    * Add asciidoc extension for custom rule block
    * Markup changes to rule title
    * modify all rules to new rule block with static rule identifiers

---
+++ asciidoc-extensions/src/main/java/be/belgif/restguide/asciidoc/extension/RuleExtension.java
@@ -0,0 +1,14 @@
+package be.belgif.restguide.asciidoc.extension;
+
+import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.extension.JavaExtensionRegistry;
+import org.asciidoctor.jruby.extension.spi.ExtensionRegistry;
+
+public class RuleExtension implements ExtensionRegistry {
+
+    public void register(Asciidoctor asciidoctor) {
+        JavaExtensionRegistry javaExtensionRegistry = asciidoctor.javaExtensionRegistry();
+        javaExtensionRegistry.block(RuleBlock.class);
+    }
+
+}
